70b4060b664c9583ec7daa7eaa145b0aa8cf69c6
Forger <maxforger@gmail.com>
2024-10-07 17:44:51 +0300

    HW-1

--- src/main/java/org/example/march2024springhw/March2024SpringHwApplication.java
+++ src/main/java/org/example/march2024springhw/March2024SpringHwApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class March2024SpringHwApplication {
+public class  March2024SpringHwApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(March2024SpringHwApplication.class, args);
